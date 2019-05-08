4b8ba15baee3202002c5f832d3088600fc98054e
Ariel Lothlorien <git@arielkauai.com>
2019-05-08 16:35:48 -0500

    This should fix the Raw Code Workarounds

--- website/src/kOSgen/kOS/workarounds.js
+++ website/src/kOSgen/kOS/workarounds.js
@@ -2,10 +2,10 @@
 Blockly.kOS['workaround_input'] = function(block) {
   var code = block.getFieldValue('CODE');
   if (code.trim().endsWith('.') == false) code = + '.'
-  return [code, Blockly.kOS.ORDER_ATOMIC];
+  return code;
 };
 Blockly.kOS['workaround_input_flow'] = function(block) {
   var code = block.getFieldValue('CODE');
   if (code.trim().endsWith('.') == false) code =+ '.'
-  return [code, Blockly.kOS.ORDER_ATOMIC];
+  return code;
 };
