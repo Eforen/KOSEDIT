71204ffd1e1533a64bc6c1b66d40d87c8af97c56
Ariel Lothlorien <git@arielkauai.com>
2019-05-08 16:17:43 -0500

    Updates

--- js/kOSall.js
+++ js/kOSall.js
@@ -7316,5 +7316,5 @@ Blockly.kOS['workaround_input'] = function(block) {
 Blockly.kOS['workaround_input_flow'] = function(block) {
   var code = block.getFieldValue('CODE');
   if (code.trim().endsWith('.') == false) code =+ '.'
-  return [code, Blockly.kOS.Blockly.kOS.ORDER_ATOMIC];
+  return [code, Blockly.kOS.ORDER_ATOMIC];
 };
